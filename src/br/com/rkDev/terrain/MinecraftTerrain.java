a5b92efe743566bde8207980024fb5137f3b9f80
Fábio Krakauer <comvocefabio@gmail.com>
2019-06-05 19:57:58 -0300

    create config and message file

--- src/br/com/rkDev/terrain/MinecraftTerrain.java
+++ src/br/com/rkDev/terrain/MinecraftTerrain.java
@@ -1,18 +1,38 @@
 package br.com.rkDev.terrain;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import br.com.rkDev.terrain.config.ConfigFile;
 
 public class MinecraftTerrain extends JavaPlugin{
 	
 	public static MinecraftTerrain INSTACE;
 	
+	private FileConfiguration config;
+	private ConfigFile messages;
+	
 	public void onEnable() {
 		INSTACE = this;
+		
+		//Start de config
+		config = getConfig();
+		saveDefaultConfig();
+		
+		messages = new ConfigFile(this, "messages.yml");
+		messages.saveConfig();
 	}
 	
 	public static MinecraftTerrain getInstance() {
 		return INSTACE;
 	}
 	
-
+	public FileConfiguration getConfig() {
+		return config;
+	}
+	
+	public ConfigFile getMessages() {
+		return messages;
+	}
+	
 }
