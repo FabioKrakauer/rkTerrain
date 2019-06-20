ab4838bbcf2bce777d1c7994a36125bb67bf45fd
Fábio <Fbio@192.168.25.236>
2019-06-20 12:47:58 -0300

    setupEconomy

--- src/br/com/rkDev/terrain/MinecraftTerrain.java
+++ src/br/com/rkDev/terrain/MinecraftTerrain.java
@@ -1,13 +1,17 @@
 package br.com.rkDev.terrain;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import br.com.rkDev.terrain.config.ConfigFile;
+import net.milkbowl.vault.economy.Economy;
 
 public class MinecraftTerrain extends JavaPlugin{
 	
 	public static MinecraftTerrain INSTACE;
+	private Economy economy;
 	
 	private FileConfiguration config;
 	private ConfigFile messages;
@@ -15,8 +19,15 @@ public class MinecraftTerrain extends JavaPlugin{
 	public void onEnable() {
 		INSTACE = this;
 		
+		if(!setupEconomy()) {
+			Bukkit.getConsoleSender().sendMessage("§c==========================================");
+			System.out.println(" ");
+			Bukkit.getConsoleSender().sendMessage("§cNao foi possível encontrar um plugin de economia compatível");
+			System.out.println(" ");
+			Bukkit.getConsoleSender().sendMessage("§c==========================================");
+		}
 		//Start de config
-		System.out.println("[rkMining] Carregando configurações...");
+		System.out.println("[rkTerrain] Carregando configurações...");
 		config = getConfig();
 		saveDefaultConfig();
 		
@@ -26,6 +37,13 @@ public class MinecraftTerrain extends JavaPlugin{
 		
 	}
 	
+	public boolean setupEconomy() {
+		RegisteredServiceProvider<Economy> provider = getServer().getServicesManager().getRegistration(Economy.class);
+		if(provider != null) {
+			economy = provider.getProvider();
+		}
+		return economy != null;
+	}
 	public static MinecraftTerrain getInstance() {
 		return INSTACE;
 	}
