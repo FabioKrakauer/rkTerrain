3d6058efdba6cbbb84f9f9220dcc74d77ffedfa2
Fábio <Fbio@192.168.25.236>
2019-06-27 11:38:40 -0300

    add invalid args

--- src/br/com/rkDev/terrain/config/Lang.java
+++ src/br/com/rkDev/terrain/config/Lang.java
@@ -5,7 +5,8 @@ import br.com.rkDev.terrain.MinecraftTerrain;
 public enum Lang {
 
 	NO_PERMISSION(MinecraftTerrain.getInstance().getMessages().getConfig().get("SemPermissao").toString()),
-	ONLY_PLAYER(MinecraftTerrain.getInstance().getMessages().getConfig().getString("Somente_Players")),;
+	ONLY_PLAYER(MinecraftTerrain.getInstance().getMessages().getConfig().getString("Somente_Players")),
+	INVALID_ARGS(MinecraftTerrain.getInstance().getMessages().getConfig().getString("Uso_Correto"));
 	
 	public String message;
 	Lang(String messag){
