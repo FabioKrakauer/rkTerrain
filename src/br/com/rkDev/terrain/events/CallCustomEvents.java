df92bc42a9c48e6428a603c7d595bd2b348ace72
FabioKrakauer <comvocefabio@gmail.com>
2019-08-03 15:45:42 -0300

    finalize custom events

--- src/br/com/rkDev/terrain/events/CallCustomEvents.java
+++ src/br/com/rkDev/terrain/events/CallCustomEvents.java
@@ -1,8 +1,12 @@
 package br.com.rkDev.terrain.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import br.com.rkDev.terrain.MinecraftTerrain;
@@ -15,12 +19,45 @@ public class CallCustomEvents implements Listener{
 		Bukkit.getPluginManager().registerEvents(this, MinecraftTerrain.getInstance());
 	}
 	@EventHandler
-	public void callJoinTerrain(PlayerMoveEvent e) {
-		Terrain instTerrain = MinecraftTerrain.getInstance().getTerrainManager().getTerrain(e.getFrom());
+	public void callJoinTerrainEvent(PlayerMoveEvent e) {
+		Terrain isntTerrain = MinecraftTerrain.getInstance().getTerrainManager().getTerrain(e.getFrom());
 		Terrain terrain = MinecraftTerrain.getInstance().getTerrainManager().getTerrain(e.getTo());
-		if(terrain != null && instTerrain == null) {
+		if(terrain != null && isntTerrain == null) {
 			User user = MinecraftTerrain.getInstance().getUserManager().getUser(e.getPlayer().getUniqueId());
 			UserJoinInTerrainEvent event = new UserJoinInTerrainEvent(user, terrain);
+			Bukkit.getPluginManager().callEvent(event);
+			e.setCancelled(event.isCancelled());
+		}
+	}
+	@EventHandler
+	public void callPvPTerrainEvent(EntityDamageByEntityEvent e) {
+		if(!(e.getDamager() instanceof Player)) {
+			return;
+		}
+		Terrain terrain = MinecraftTerrain.getInstance().getTerrainManager().getTerrain(e.getEntity().getLocation());
+		if(terrain != null) {
+			User user = MinecraftTerrain.getInstance().getUserManager().getUser(e.getDamager().getUniqueId());
+			UserHitInTerrainEvent event = new UserHitInTerrainEvent(user, terrain);
+			Bukkit.getPluginManager().callEvent(event);
+			e.setCancelled(event.isCancelled());
+		}
+	}
+	@EventHandler
+	public void callBuildTerrainEvent(BlockPlaceEvent e) {
+		Terrain terrain = MinecraftTerrain.getInstance().getTerrainManager().getTerrain(e.getBlock().getLocation());
+		if(terrain != null) {
+			User user = MinecraftTerrain.getInstance().getUserManager().getUser(e.getPlayer().getUniqueId());
+			UserBuildInTerrainEvent event = new UserBuildInTerrainEvent(user, terrain);
+			Bukkit.getPluginManager().callEvent(event);
+			e.setCancelled(event.isCancelled());
+		}
+	}
+	@EventHandler
+	public void callBuildTerrainEvent(BlockBreakEvent e) {
+		Terrain terrain = MinecraftTerrain.getInstance().getTerrainManager().getTerrain(e.getBlock().getLocation());
+		if(terrain != null) {
+			User user = MinecraftTerrain.getInstance().getUserManager().getUser(e.getPlayer().getUniqueId());
+			UserBuildInTerrainEvent event = new UserBuildInTerrainEvent(user, terrain);
 			Bukkit.getPluginManager().callEvent(event);
 			e.setCancelled(event.isCancelled());
 		}
---
+++ src/br/com/rkDev/terrain/events/UserHitInTerrainEvent.java
@@ -0,0 +1,49 @@
+package br.com.rkDev.terrain.events;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import br.com.rkDev.terrain.manage.Terrain;
+import br.com.rkDev.terrain.user.User;
+
+
+public class UserHitInTerrainEvent extends Event implements Cancellable{
+
+	private User user;
+	private Terrain terrain;
+	
+	private boolean cancelled;
+	public UserHitInTerrainEvent(User user, Terrain terrain) {
+		this.user = user;
+		this.terrain = terrain;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	public Terrain getTerrain() {
+		return terrain;
+	}
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+		
+	}
+	
+	private static final HandlerList handlers = new HandlerList();
+	 
+	@Override
+	public HandlerList getHandlers() {
+	    return handlers;
+	}
+	 
+	public static HandlerList getHandlerList() {
+	    return handlers;
+	}
+}
